160d4580d537274d41363e1fdfcb2ff26bec1d49
Matt Brealey <matt.brealey@filigree.tech>
2020-03-19 15:56:35 +0000

    Add footer links

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,9 +1,9 @@
 <footer class="text-center text-xs text-gray-500 p-4">
     <p>This site is currently being written using
-    <a href="#" class="colorLink">Sapper</a>
+    <a href="https://sapper.svelte.dev/" target="_blank" rel="noopener noreferrer" class="colorLink">Sapper</a>
     and
-    <a href="#" class="colorLink">TailwindCSS</a>
+    <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" class="colorLink">TailwindCSS</a>
     , and is entirely
-    <a href="#" class="colorLink">open source</a>
+    <a href="https://github.com/badgrenola/matt-site" target="_blank" rel="noopener noreferrer" class="colorLink">open source</a>
     👍</p>
 </footer> 
