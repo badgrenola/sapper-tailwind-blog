f0d90fab7507a05f4c39ab132a0556d8197803d3
Matt Brealey <matt.brealey@filigree.tech>
2020-03-19 10:55:29 +0000

    Add highlighting on active route link

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,18 +1,28 @@
 <script>
+    //Grab the active route
+    import { stores } from '@sapper/app'
+    const { page } = stores()
+    let isIndex; // TODO : svelte should declare these for us in the $: statement, but I'm getting undeclared errors 
+    let isProjects;
+    let isWriting;
+    $: {
+        isIndex = $page.path === "/"
+        isProjects = $page.path === "/projects"
+        isWriting = $page.path === "/writing"
+    }
+
+    //Store the mobile menu open state
     var isOpen = false;
 
-    function handleOnClick() {
-        isOpen = !isOpen
-    }
 </script>
 
-<header class="bg-white sm:flex sm:justify-between sm:items-center sm:p-4 lg:p-8">
+<header class="sm:flex sm:justify-between sm:items-center sm:p-4 lg:p-8">
     <div class="flex items-center justify-between px-4 py-4 sm:p-0">
         <div class="">
             <img class="h-12 rounded-full" src="profile.jpg" alt="A picture of Matt Brealey's head" />
         </div>
         <div class="sm:hidden">
-            <button type="button" id="menuButton" class="block text-black hover:text-gray-800 focus:text-gray-800 focus:outline-none pr-2" on:click={handleOnClick}>
+            <button type="button" id="menuButton" class="block text-black hover:text-gray-800 focus:text-gray-800 focus:outline-none pr-2" on:click={() => {isOpen = !isOpen}}>
                 <svg class="h-8 w-8 fill-current {isOpen ? 'text-pink-600' : 'text-black'}" viewBox="0 0 100 70">
                     <rect width="100" height="10"></rect>
                     <rect y="30" width="100" height="10"></rect>
@@ -22,8 +32,8 @@
         </div>
     </div>
     <div class="px-2 pt-2 pb-4 {isOpen ? 'block' : 'hidden'} sm:flex sm:p-0">
-        <a href="/" class="block mt-1 px-2 text-pink-600 font-semibold text-center hover:text-pink-500 hover:underline sm:mt-0 sm:ml-2 sm:font-normal sm:text-sm">Hello</a>
-        <a href="/about" id="projectsMenuItem" class="block mt-1 px-2 text-pink-600 font-semibold text-center hover:text-pink-500 hover:underline sm:mt-0 sm:ml-2 sm:font-normal sm:text-sm">Projects</a>
-        <a href="/blog" class="block mt-1 px-2 text-pink-600 font-semibold text-center hover:text-pink-500 hover:underline sm:mt-0 sm:ml-2 sm:font-normal sm:text-sm">Writing</a>
+        <a href="/" class="block text-pink-500 opacity-75 font-semibold text-center hover:opacity-100 hover:underline sm:mt-0 sm:ml-2 sm:font-normal sm:text-sm sm:px-2 h-8 sm:h-auto {isIndex ? " opacity-100 underline cursor-default" : ""}">Hello</a>
+        <a href="/about" id="projectsMenuItem" class="block text-pink-500 opacity-75 font-semibold text-center hover:opacity-100 hover:underline sm:mt-0 sm:ml-2 sm:font-normal sm:text-sm sm:px-2 h-8 sm:h-auto {isProjects ? " opacity-100 underline cursor-default" : ""}">Projects</a>
+        <a href="/blog" class="block text-pink-500 opacity-75 font-semibold text-center hover:opacity-100 hover:underline sm:mt-0 sm:ml-2 sm:font-normal sm:text-sm sm:px-2 h-8 sm:h-auto {isWriting ? " opacity-100 underline cursor-default" : ""}">Writing</a>
     </div>
 </header>
