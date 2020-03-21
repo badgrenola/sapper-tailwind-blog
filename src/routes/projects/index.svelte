1bb3fa8509b2c067bff29a0f60ccb826f305f7e7
Matt Brealey <matt.brealey@filigree.tech>
2020-03-21 18:35:13 +0000

    Add dynamic project page using [slug] // Add extra params to the projectStore

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -17,12 +17,12 @@
 			{#each $projects as project}
 				<div class="my-2 mb-4 px-2 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-full md:w-1/2 xl:w-1/3">
 					<div class="group flex flex-col relative h-full cursor-pointer">
-						<a href="{project.path}" class="absolute w-full h-full z-10"> </a>
+						<a rel="prefetch" href="projects/{project.slug}" class="absolute w-full h-full z-10"> </a>
 						<div class="relative pb-2/3 rounded-md overflow-hidden opacity-75 group-hover:opacity-100 transition-opacity duration-100 ease-in-out">
-							<img class="absolute h-full w-full object-cover" src="./testImage.jpg" alt="{project.title}" />
+							<img class="absolute h-full w-full object-cover" src={project.smallImage} alt={project.title} />
 						</div>
 						<div class="font-semibold mt-1 text-gray-900 group-hover:text-pink-500">{project.title}</div>
-						<span class="text-sm text-gray-800">{project.desc}</span>
+						<span class="text-sm text-gray-800">{project.shortDesc}</span>
 					</div>
 				</div>
 			{/each}
--- src/routes/projects/juno-observer.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import PageLayout from '../../components/pageLayout.svelte'
-</script>
-
-<PageLayout>
-    <span slot="title">Juno Observer</span>
-</PageLayout>
