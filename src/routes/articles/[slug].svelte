2d5051769c1423043f2e7fcf08e8c2cf93cc8512
Matt Brealey <matt.brealey@filigree.tech>
2020-03-22 18:43:52 +0000

    Hide HR at bottom of articles if no prev/next

--- src/routes/articles/[slug].svelte
+++ src/routes/articles/[slug].svelte
@@ -33,21 +33,23 @@
 			{@html article.html}
 		</span>
 		
-		<hr class="mt-12 w-full"/>
-		<div class="flex justify-between pt-2">
-			<div class="flex-1 pr-6">
-				{#if article.prev}
-					<h3 class="text-sm text-gray-600">Previous Article</h3>
-					<a href="./articles/{article.prev.slug}" class="colorLink">{article.prev.title}</a>
-				{/if}
+		{#if (article.prev || article.next)}
+			<hr class="mt-12 w-full"/>
+			<div class="flex justify-between pt-2">
+				<div class="flex-1 pr-6">
+					{#if article.prev}
+						<h3 class="text-sm text-gray-600">Previous Article</h3>
+						<a href="./articles/{article.prev.slug}" class="colorLink">{article.prev.title}</a>
+					{/if}
+				</div>
+				<div class="flex-1 text-right pl-6">
+					{#if article.next}
+						<h3 class="text-sm text-gray-600">Next Article</h3>
+						<a href="./articles/{article.next.slug}" class="colorLink">{article.next.title}</a>
+					{/if}
+				</div>
 			</div>
-			<div class="flex-1 text-right pl-6">
-				{#if article.next}
-					<h3 class="text-sm text-gray-600">Next Article</h3>
-					<a href="./articles/{article.next.slug}" class="colorLink">{article.next.title}</a>
-				{/if}
-			</div>
-		</div>
+		{/if}
 
 	</section>
 </PageLayout>    
