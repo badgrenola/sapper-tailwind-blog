a28fc9d5f69490c8c3c5fb2a7e67c162612690a0
Matt Brealey <matt.brealey@filigree.tech>
2020-03-24 17:21:07 +0000

    Add projects from session to sidebar // Remove project store

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,12 +2,18 @@
 <!-- https://github.com/sveltejs/sapper/issues/703 -->
 
 <script context="module">
-	export async function preload({ params, query }) {
-    return this.fetch(`articles.json`)
-    .then(r => r.json())
-    .then(articles => {
-		return { articles };
-	});
+	export async function preload({ params, query }, session) {
+		return this.fetch(`articles.json`)
+			.then(r => r.json())
+			.then(articles => {
+
+				//Populate projects from backend
+				const projects = Object.keys(session.projects).map(projectSlug => session.projects[projectSlug])
+				return { 
+					articles,
+					projects
+				};
+		});
 	}
 </script>
 
@@ -16,6 +22,7 @@
 	import Header from '../components/header.svelte';
 	import Sidebar from '../components/sidebar.svelte';
 	import Footer from '../components/footer.svelte';
+	export var projects;
 	export var articles;
 	export var segment;
 </script>
@@ -25,7 +32,7 @@
 
 	<main class="flex-grow pb-6">
 		<div class="font-normal text-gray-900 text-base sm:text-base p-6 flex lg:p-8">
-			<Sidebar articles={articles} />
+			<Sidebar articles={articles} projects={projects} />
 			<div class="w-full sm:w-4/5 font-light">
 				<slot></slot>
 			</div>
