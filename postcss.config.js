f2f7191702fa916da0f3556c6a368ecf254bf364
Matt Brealey <matt.brealey@filigree.tech>
2020-03-26 17:04:53 +0000

    Add CSSNano

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,6 @@
+const production = process.env.NODE_ENV === "production";
+const cssnano = require('cssnano');
+
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.svelte", "./src/**/*.html"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
@@ -6,7 +9,11 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-    require('autoprefixer')
+    ...(production ? [purgecss] : []),
+    require('autoprefixer'),
+    cssnano({
+      autoprefixer: false,
+      preset: ['default'],
+   }),
   ]
 };
